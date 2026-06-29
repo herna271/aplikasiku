0c1d1d9ea28969185537902bc44339f0228987c9
Herna Putri Kusuma <hernaputrikusuma73@gmail.com>
2026-06-29 20:07:05 +0700

    Commit2

---
+++ database/migrations/2026_06_26_120228_add_lecturer_id_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('lecturer_id')
+                ->nullable()
+                ->after('id')
+                ->constrained('lecturers')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['lecturer_id']);
+            $table->dropColumn('lecturer_id');
+        });
+    }
+};
